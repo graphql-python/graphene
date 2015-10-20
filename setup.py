551a0d0b1d3c0f335d9cb62f542b21dedf53b5c3
Syrus Akbary <me@syrusakbary.com>
2015-10-19 23:04:07 -0700

    Improved version (using alpha status)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 setup(
     name='graphene',
-    version='0.1.5.3',
+    version='0.1.6a1',
 
     description='Graphene: Python DSL for GraphQL',
     long_description=open('README.rst').read(),
