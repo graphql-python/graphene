25c59833ccc877c7f8258d0e8dcfabace11f19ce
Amit Saha <asaha@freelancer.com>
2015-11-20 11:12:46 +1100

    Example of using a Python class as a field

---
+++ examples/field_example.py
@@ -0,0 +1,30 @@
+import graphene
+
+class Person(graphene.Interface):
+    name = graphene.String()
+    age = graphene.ID()
+
+class Patron(Person):
+    id = graphene.ID()
+
+class Query(graphene.ObjectType):
+
+    patron = graphene.Field(Patron)
+
+    def resolve_patron(self, args, info):
+        return Patron(id=1, name='Demo')
+
+schema = graphene.Schema(query=Query)
+query = '''
+    query something{
+      patron {
+        id
+        name
+      }
+}
+'''
+result = schema.execute(query)
+# Print the result
+print result.data['patron']
+
+
