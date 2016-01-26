6a38024340a58d606f6b6d6eee2fb26565aa2519
Syrus Akbary <me@syrusakbary.com>
2016-01-26 11:52:11 -0800

    Updated version to 0.7.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 setup(
     name='graphene',
-    version='0.6.1',
+    version='0.7.0',
 
     description='GraphQL Framework for Python',
     long_description=open('README.rst').read(),
