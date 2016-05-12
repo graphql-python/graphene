4eb70bca00fadb62a9618b45e68807dd3881c46b
Syrus Akbary <me@syrusakbary.com>
2016-05-12 00:09:53 -0700

    Updated graphene version to 0.9b1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 setup(
     name='graphene',
-    version='0.8.2',
+    version='0.9b1',
 
     description='GraphQL Framework for Python',
     long_description=open('README.rst').read(),
