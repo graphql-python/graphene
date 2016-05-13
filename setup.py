398088a0c496937f0f3fe3d8dc04c8d163767723
Syrus Akbary <syrus@affirm.com>
2016-05-13 13:45:22 -0700

    Updated version to 0.9.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
 setup(
     name='graphene',
-    version='0.9',
+    version='0.9.1',
 
     description='GraphQL Framework for Python',
     long_description=open('README.rst').read(),
