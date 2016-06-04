9cf4da5fcb821019ec871755927fa92cd56f12bf
Syrus Akbary <me@syrusakbary.com>
2016-06-04 14:14:56 -0700

    Improved simple example

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -4,15 +4,15 @@ import graphene
 class Patron(graphene.ObjectType):
     id = graphene.ID()
     name = graphene.String()
-    age = graphene.ID()
+    age = graphene.Int()
 
 
 class Query(graphene.ObjectType):
 
     patron = graphene.Field(Patron)
 
-    def resolve_patron(self, args, info):
-        return Patron(id=1, name='Demo')
+    def resolve_patron(self, args, context, info):
+        return Patron(id=1, name='Syrus', age=27)
 
 schema = graphene.Schema(query=Query)
 query = '''
@@ -20,8 +20,24 @@ query = '''
       patron {
         id
         name
+        age
       }
     }
 '''
-result = schema.execute(query)
-print(result.data['patron'])
+
+
+def test_query():
+    result = schema.execute(query)
+    assert not result.errors
+    assert result.data == {
+        'patron': {
+            'id': '1',
+            'name': 'Syrus',
+            'age': 27,
+        }
+    }
+
+
+if __name__ == '__main__':
+    result = schema.execute(query)
+    print(result.data['patron'])
