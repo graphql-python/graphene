40705d3924e11d7778d269736a786f2782703306
Markus Padourek <markus.padourek@gmail.com>
2016-06-08 17:30:30 +0100

    Fix import

--- graphene/relay/types.py
+++ graphene/relay/types.py
@@ -5,7 +5,7 @@ from functools import wraps
 
 import six
 
-from graphql_relay.connection.arrayconnection import connection_from_list
+from graphql_relay.connection.arrayconnection import connection_from_list_slice
 from graphql_relay.node.node import to_global_id
 
 from ..core.classtypes import InputObjectType, Interface, Mutation, ObjectType
