76ecd895e1db70aa0b30aa665160553fc83ace63
Syrus Akbary <me@syrusakbary.com>
2016-06-14 22:26:18 -0700

    Removed unused code

--- graphene/utils/tests/test_get_fields.py
+++ graphene/utils/tests/test_get_fields.py
@@ -1,11 +1,11 @@
 from collections import OrderedDict
 from graphql import GraphQLField, GraphQLString, GraphQLInterfaceType, GraphQLInt, GraphQLFloat
-from ..extract_fields import extract_fields, get_base_fields
+from ..get_fields import get_fields_from_attrs, get_fields_from_types
 
 from ...types import Field, String, Argument, ObjectType
 
 
-def test_extract_fields_attrs():
+def test_get_fields_from_attrs():
     attrs = {
         'field_string': Field(String),
         'string': String(),
@@ -13,27 +13,26 @@ def test_extract_fields_attrs():
         'argument': Argument(String),
         'graphql_field': GraphQLField(GraphQLString)
     }
-    extracted_fields = list(extract_fields(ObjectType, attrs))
-    assert [f.name for f in extracted_fields] == ['fieldString', 'string']
-    assert sorted(attrs.keys()) == ['argument', 'graphql_field', 'other']
+    extracted_fields = OrderedDict(get_fields_from_attrs(ObjectType, attrs))
+    assert [f for f in extracted_fields.keys()] == ['field_string', 'string']
 
 
-def test_extract_fields():
+def test_get_fields_from_types():
     int_base = GraphQLInterfaceType('IntInterface', fields=OrderedDict([
         ('int', GraphQLField(GraphQLInt)),
         ('num', GraphQLField(GraphQLInt)),
         ('extra', GraphQLField(GraphQLInt))
     ]))
-    float_base = GraphQLInterfaceType('IntInterface', fields=OrderedDict([
+    float_base = GraphQLInterfaceType('FloatInterface', fields=OrderedDict([
         ('float', GraphQLField(GraphQLFloat)),
         ('num', GraphQLField(GraphQLFloat)),
         ('extra', GraphQLField(GraphQLFloat))
     ]))
 
     bases = (int_base, float_base)
-    base_fields = list(get_base_fields(ObjectType, bases))
-    assert [f.name for f in base_fields] == ['int', 'num', 'extra', 'float']
-    assert [f.type for f in base_fields] == [
+    base_fields = OrderedDict(get_fields_from_types(bases))
+    assert [f for f in base_fields.keys()] == ['int', 'num', 'extra', 'float']
+    assert [f.type for f in base_fields.values()] == [
         GraphQLInt,
         GraphQLInt,
         GraphQLInt,
