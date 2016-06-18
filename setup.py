c88e933e9e63bea8c20baac4ed9257ecfcbfa1c6
Syrus Akbary <me@syrusakbary.com>
2016-06-18 14:31:33 -0700

    Update graphql_relay to last version

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     install_requires=[
         'six>=1.10.0',
         'graphql-core>=0.5.1',
-        'graphql-relay>=0.4.3',
+        'graphql-relay>=0.4.4',
         'iso8601',
     ],
     tests_require=[
