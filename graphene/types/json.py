043e548f498cbc2bb83c7fa691bbaebc039cef88
Syrus Akbary <me@syrusakbary.com>
2016-06-21 22:07:16 -0700

    Added JSONString

---
+++ graphene/types/json.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+import json
+from graphql.language import ast
+
+from .scalars import Scalar
+
+
+class JSONString(Scalar):
+    '''JSON String'''
+
+    @staticmethod
+    def serialize(dt):
+        return json.dumps(dt)
+
+    @staticmethod
+    def parse_literal(node):
+        if isinstance(node, ast.StringValue):
+            return json.dumps(node.value)
+
+    @staticmethod
+    def parse_value(value):
+        return json.dumps(value)
