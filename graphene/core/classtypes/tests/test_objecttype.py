a7d7f95b86f004f6d12b38f58e91b8885538ebb4
Markus Padourek <markus.padourek@gmail.com>
2016-07-26 12:16:48 +0100

    fix linting issues

--- graphene/core/classtypes/tests/test_objecttype.py
+++ graphene/core/classtypes/tests/test_objecttype.py
@@ -54,15 +54,17 @@ def test_object_type_set_properties():
     assert h.readonly_prop == 'readonly'
     assert h.write_prop == 'custom'
 
+
 def test_object_type_set_private_attributes():
     class Human(ObjectType):
         _private_state = None
-    
+
     h = Human(_private_state='custom')
     assert h._private_state == 'custom'
-    
+
     with raises(TypeError):
         Human(_other_private_state='My name')
+
 
 def test_object_type_container_invalid_kwarg():
     class Human(ObjectType):
