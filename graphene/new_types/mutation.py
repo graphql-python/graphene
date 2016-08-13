9c4706f7c5feefb61f65cb19bfffe7b0a2ccf39d
Syrus Akbary <me@syrusakbary.com>
2016-08-12 18:37:29 -0700

    Added Mutation

---
+++ graphene/new_types/mutation.py
@@ -0,0 +1,30 @@
+from functools import partial
+
+import six
+
+from ..utils.is_base_type import is_base_type
+from ..utils.props import props
+from .field import Field
+from .objecttype import ObjectType, ObjectTypeMeta
+
+
+class MutationMeta(ObjectTypeMeta):
+
+    def __new__(cls, name, bases, attrs):
+        # Also ensure initialization is only performed for subclasses of
+        # Mutation
+        if not is_base_type(bases, MutationMeta):
+            return type.__new__(cls, name, bases, attrs)
+
+        input_class = attrs.pop('Input', None)
+
+        cls = ObjectTypeMeta.__new__(cls, name, bases, attrs)
+        field_args = props(input_class) if input_class else {}
+        resolver = getattr(cls, 'mutate', None)
+        assert resolver, 'All mutations must define a mutate method in it'
+        cls.Field = partial(Field, cls, args=field_args, resolver=resolver)
+        return cls
+
+
+class Mutation(six.with_metaclass(MutationMeta, ObjectType)):
+    pass
