1820a4eb60f58969a4f2ab769675697dacf94ad5
Syrus Akbary <me@syrusakbary.com>
2016-08-16 00:02:16 -0700

    Updated subpackages dependency

--- graphene-django/setup.py
+++ graphene-django/setup.py
@@ -33,7 +33,7 @@ setup(
 
     install_requires=[
         'six>=1.10.0',
-        'graphene>=1.0',
+        'graphene>=1.0.dev',
         'Django>=1.6.0',
         'iso8601',
         'singledispatch>=3.4.0.3',
--- graphene-sqlalchemy/setup.py
+++ graphene-sqlalchemy/setup.py
@@ -33,7 +33,7 @@ setup(
 
     install_requires=[
         'six>=1.10.0',
-        'graphene>=1.0',
+        'graphene>=1.0.dev',
         'SQLAlchemy',
         'singledispatch>=3.4.0.3',
     ],
