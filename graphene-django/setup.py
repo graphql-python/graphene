210a2aed49d78f5ab2502d3828db724ffa991d0b
Syrus Akbary <me@syrusakbary.com>
2016-09-10 21:25:00 -0700

    Updated Graphene Django version

--- graphene-django/setup.py
+++ graphene-django/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphene-django',
-    version='1.0.dev20160909000001',
+    version='1.0.dev20160910000001',
 
     description='Graphene Django integration',
     # long_description=open('README.rst').read(),
