fc43421e68afb0f0cd16dfe6ad8c25c119058fa1
Syrus Akbary <me@syrusakbary.com>
2016-09-21 00:20:40 -0700

    Removed docs layout

--- docs/conf.py
+++ docs/conf.py
@@ -134,6 +134,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+if on_rtd:
+    html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
