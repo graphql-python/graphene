1538a2cc6e6a9b7243f0b95c8f4fafdefa489de8
Tony Angerilli <tony@7geese.com>
2016-11-15 12:03:10 -0800

    add a test

--- graphene/types/tests/test_union.py
+++ graphene/types/tests/test_union.py
@@ -1,7 +1,9 @@
 import pytest
 
 from ..objecttype import ObjectType
+from ..schema import Schema
 from ..union import Union
+from graphene.relay.connection import ConnectionField
 
 
 class MyObjectType1(ObjectType):
@@ -41,3 +43,33 @@ def test_generate_union_with_no_types():
             pass
 
     assert str(exc_info.value) == 'Must provide types for Union MyUnion.'
+
+
+def test_union_as_connection():
+    class MyUnion(Union):
+        class Meta:
+            types = (MyObjectType1, MyObjectType2)
+
+    class Query(ObjectType):
+        objects = ConnectionField(MyUnion)
+
+        def resolve_objects(self, args, context, info):
+            return [MyObjectType1(), MyObjectType2()]
+
+    query = '''
+    query {
+        objects {
+            edges {
+                node {
+                    __typename
+                }
+            }
+        }
+    }
+    '''
+    schema = Schema(query=Query)
+    result = schema.execute(query)
+    assert not result.errors
+    assert len(result.data['objects']['edges']) == 2
+    assert result.data['objects']['edges'][0]['node']['__typename'] == 'MyObjectType1'
+    assert result.data['objects']['edges'][1]['node']['__typename'] == 'MyObjectType2'
