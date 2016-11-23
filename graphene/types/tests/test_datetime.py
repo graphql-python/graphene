48b422e2890e02801a6b3a993f28a0b696da797c
Paul Bailey <paul.m.bailey@gmail.com>
2016-11-23 11:12:28 -0500

    add time query test

--- graphene/types/tests/test_datetime.py
+++ graphene/types/tests/test_datetime.py
@@ -1,17 +1,21 @@
 import datetime
 import pytz
 
-from ..datetime import DateTime
+from ..datetime import DateTime, Time
 from ..objecttype import ObjectType
 from ..schema import Schema
 
 
 class Query(ObjectType):
     datetime = DateTime(_in=DateTime(name='in'))
+    time = Time(_at=Time(name='at'))
 
     def resolve_datetime(self, args, context, info):
         _in = args.get('in')
         return _in
+
+    def resolve_time(self, args, context, info):
+        return args.get('at')
 
 schema = Schema(query=Query)
 
@@ -26,6 +30,17 @@ def test_datetime_query():
         'datetime': isoformat
     }
 
+
+def test_time_query():
+    now = datetime.datetime.now().replace(tzinfo=pytz.utc)
+    time = datetime.time(now.hour, now.minute, now.second, now.microsecond, now.tzinfo)
+    isoformat = time.isoformat()
+
+    result = schema.execute('''{ time(at: "%s") }'''%isoformat)
+    assert not result.errors
+    assert result.data == {
+        'time': isoformat
+    }
 
 def test_datetime_query_variable():
     now = datetime.datetime.now().replace(tzinfo=pytz.utc)
