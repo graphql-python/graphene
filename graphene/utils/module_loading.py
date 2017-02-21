1a52cf9a3d5d6b8c6159a53df6f5e57ad34066ed
Syrus Akbary <me@syrusakbary.com>
2017-02-20 21:59:38 -0800

    Fixed lint

--- graphene/utils/module_loading.py
+++ graphene/utils/module_loading.py
@@ -9,14 +9,14 @@ def import_string(dotted_path):
     """
     try:
         module_path, class_name = dotted_path.rsplit('.', 1)
-    except ValueError as err:
+    except ValueError:
         raise ImportError("%s doesn't look like a module path" % dotted_path)
 
     module = import_module(module_path)
 
     try:
         return getattr(module, class_name)
-    except AttributeError as err:
+    except AttributeError:
         raise ImportError('Module "%s" does not define a "%s" attribute/class' % (
             module_path, class_name)
         )
