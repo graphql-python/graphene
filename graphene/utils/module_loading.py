087f1c55cd99d591ac99595beed9ac8c2e8f3fe5
Yue Gong <blakegong@gmail.com>
2017-03-25 17:41:47 +0800

    Improve lazy_import() to accept dotted_attributes

--- graphene/utils/module_loading.py
+++ graphene/utils/module_loading.py
@@ -2,7 +2,7 @@ from functools import partial
 from importlib import import_module
 
 
-def import_string(dotted_path):
+def import_string(dotted_path, dotted_attributes=None):
     """
     Import a dotted module path and return the attribute/class designated by the
     last name in the path. Raise ImportError if the import failed.
@@ -15,12 +15,27 @@ def import_string(dotted_path):
     module = import_module(module_path)
 
     try:
-        return getattr(module, class_name)
+        result = getattr(module, class_name)
     except AttributeError:
         raise ImportError('Module "%s" does not define a "%s" attribute/class' % (
             module_path, class_name)
         )
 
+    if not dotted_attributes:
+        return result
+    else:
+        attributes = dotted_attributes.split('.')
+        traveled_attributes = []
+        try:
+            for attribute in attributes:
+                traveled_attributes.append(attribute)
+                result = getattr(result, attribute)
+            return result
+        except AttributeError:
+            raise ImportError('Module "%s" does not define a "%s" attribute inside attribute/class "%s"' % (
+                module_path, '.'.join(traveled_attributes), class_name
+            ))
 
-def lazy_import(dotted_path):
-    return partial(import_string, dotted_path)
+
+def lazy_import(dotted_path, dotted_attributes=None):
+    return partial(import_string, dotted_path, dotted_attributes)
