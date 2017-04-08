afed25a18015643749439288a9536235232dd493
Syrus Akbary <me@syrusakbary.com>
2017-04-07 22:02:16 -0700

    Improved enums comparison and getters. Fixed #444

--- graphene/types/enum.py
+++ graphene/types/enum.py
@@ -13,6 +13,12 @@ except ImportError:
     from ..pyutils.enum import Enum as PyEnum
 
 
+def eq_enum(self, other):
+    if isinstance(other, self.__class__):
+        return self is other
+    return self.value is other
+
+
 class EnumTypeMeta(type):
 
     def __new__(cls, name, bases, attrs):
@@ -28,6 +34,7 @@ class EnumTypeMeta(type):
             enum=None,
         )
         if not options.enum:
+            attrs['__eq__'] = eq_enum
             options.enum = PyEnum(cls.__name__, attrs)
 
         new_attrs = OrderedDict(attrs, _meta=options, **options.enum.__members__)
@@ -36,11 +43,18 @@ class EnumTypeMeta(type):
     def __prepare__(name, bases, **kwargs):  # noqa: N805
         return OrderedDict()
 
+    def get(cls, value):
+        return cls._meta.enum(value)
+
+    def __getitem__(cls, value):
+        return cls._meta.enum[value]
+
     def __call__(cls, *args, **kwargs):  # noqa: N805
         if cls is Enum:
             description = kwargs.pop('description', None)
             return cls.from_enum(PyEnum(*args, **kwargs), description=description)
         return super(EnumTypeMeta, cls).__call__(*args, **kwargs)
+        # return cls._meta.enum(*args, **kwargs)
 
     def from_enum(cls, enum, description=None):  # noqa: N805
         meta_class = type('Meta', (object,), {'enum': enum, 'description': description})
