faf8a41118555c715c3a56b8371ab33b654fe9ab
Adam Donahue <adam.donahue@loan-street.com>
2017-06-27 18:22:22 -0400

    Fix to comments.

--- graphene/utils/str_converters.py
+++ graphene/utils/str_converters.py
@@ -5,7 +5,7 @@ def to_camel_case(snake_str):
     """
     Return a camel-cased version of a snake-cased string.
 
-    Leading underscores and multiple-underscores are kept
+    Leading underscores and multiple consecutive underscores are kept
     intact.
 
     :param snake_str: A snake-cased string.
@@ -16,13 +16,13 @@ def to_camel_case(snake_str):
     # component.
     snake_case_sub_strings = re.findall(r'(_*[a-zA-Z]+|_+$)', snake_str)
 
-    # The first variable is unchanged case wise (and leading
+    # The first variable is unchanged case-wise (and leading
     # underscores preserved as-is).
     camel_case_sub_strings = [snake_case_sub_strings[0]]
 
     for s in snake_case_sub_strings[1:]:
         # We reset the camel casing algorithm if more than one
-        # underscore is encountered.  The endwiths handles any
+        # underscore is encountered.  The endswith handles any
         # trailing underscores in the original snake-cased
         # variable.
         if s.startswith('__') or s.endswith('_'):
