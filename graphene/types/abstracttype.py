85d31458624f809d38ee3c79c21dc7d55debd326
Syrus Akbary <me@syrusakbary.com>
2017-07-23 19:13:33 -0700

    Improved abstract type

--- graphene/types/abstracttype.py
+++ graphene/types/abstracttype.py
@@ -1,9 +1,12 @@
-from ..pyutils.init_subclass import InitSubclassMeta
+from ..utils.subclass_with_meta import SubclassWithMeta
+from ..utils.deprecated import warn_deprecation
 
 
-class AbstractType(object):
-    __metaclass__ = InitSubclassMeta
+class AbstractType(SubclassWithMeta):
 
     def __init_subclass__(cls, *args, **kwargs):
-        print("Abstract type is deprecated")
-        # super(AbstractType, cls).__init_subclass__(*args, **kwargs)
+        warn_deprecation(
+            "Abstract type is deprecated, please use normal object inheritance instead.\n"
+            "See more: https://github.com/graphql-python/graphene/blob/2.0/UPGRADE-v2.0.md#deprecations"
+        )
+        super(AbstractType, cls).__init_subclass__(*args, **kwargs)
