7e901f83aeed1ae0b06c120f19c5dbf95dccc1ce
Syrus Akbary <me@syrusakbary.com>
2017-08-01 23:39:27 -0700

    Improved test coverage

--- graphene/utils/tests/test_resolve_only_args.py
+++ graphene/utils/tests/test_resolve_only_args.py
@@ -4,10 +4,12 @@ from .. import deprecated
 
 def test_resolve_only_args(mocker):
     mocker.patch.object(deprecated, 'warn_deprecation')
-    def resolver(*args, **kwargs):
-        return kwargs
+    def resolver(root, **args):
+        return root, args
 
     my_data = {'one': 1, 'two': 2}
 
-    wrapped = resolve_only_args(resolver)
+    wrapped_resolver = resolve_only_args(resolver)
     assert deprecated.warn_deprecation.called
+    result = wrapped_resolver(1, 2, a=3)
+    assert result == (1, {'a': 3})
