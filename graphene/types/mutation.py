368a2a02fe38be577c7274cdd3e0788a2b2155b5
Pi Delport <pjdelport@gmail.com>
2018-02-08 01:26:27 +0200

    (Fit line length < 120)

--- graphene/types/mutation.py
+++ graphene/types/mutation.py
@@ -50,7 +50,8 @@ class Mutation(ObjectType):
                     warn_deprecation((
                         "Please use {name}.Arguments instead of {name}.Input."
                         "Input is now only used in ClientMutationID.\n"
-                        "Read more: https://github.com/graphql-python/graphene/blob/v2.0.0/UPGRADE-v2.0.md#mutation-input"
+                        "Read more:"
+                        " https://github.com/graphql-python/graphene/blob/v2.0.0/UPGRADE-v2.0.md#mutation-input"
                     ).format(name=cls.__name__))
 
             if input_class:
