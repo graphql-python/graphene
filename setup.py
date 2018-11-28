2c43a2ae0a04fc61ecf68e9b6f7df8c6fbaae886
as <andreas.sodeur@rwe.com>
2018-11-28 17:58:19 +0100

    Now setup.py works on Windows other tools have trouble with PKG-INFO. Forcing long_description to ASCII.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     name="graphene",
     version=version,
     description="GraphQL Framework for Python",
-    long_description=codecs.open("README.rst", "r", "utf-8").read(),
+    long_description=codecs.open("README.rst", "r", encoding="ascii", errors="replace").read(),
     url="https://github.com/graphql-python/graphene",
     author="Syrus Akbary",
     author_email="me@syrusakbary.com",
