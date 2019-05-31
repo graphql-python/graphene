bd2f4a3449551ed056f8d11a15696a7b957428da
Eran Kampf <ekampf@ebates.com>
2019-05-31 12:32:07 -0700

    Replicate error with test

---
+++ tests_asyncio/test_objecttype.py
@@ -0,0 +1,19 @@
+import pytest
+
+from graphene import Schema, ObjectType, String
+
+
+def test_objecttype_meta_with_annotations():
+    class Query(ObjectType):
+        class Meta:
+            name: str = 'oops'
+
+        hello = String()
+
+        def resolve_hello(self, info):
+            return 'Hello'
+
+    schema = Schema(query=Query)
+    assert schema is not None
+
+
