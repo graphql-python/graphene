b06f8446064ceacf92b3aed6549264fdc848b2d6
Eran Kampf <ekampf@ebates.com>
2019-06-01 14:46:43 -0700

    Python3 tests no longer need a separate folder

--- graphene/types/tests/test_objecttype.py
+++ graphene/types/tests/test_objecttype.py
@@ -265,3 +265,17 @@ def test_abstract_objecttype_can_str():
         field = MyScalar()
 
     assert str(MyObjectType) == "MyObjectType"
+
+
+def test_objecttype_meta_with_annotations():
+    class Query(ObjectType):
+        class Meta:
+            name: str = "oops"
+
+        hello = String()
+
+        def resolve_hello(self, info):
+            return "Hello"
+
+    schema = Schema(query=Query)
+    assert schema is not None
