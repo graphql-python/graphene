b0ee84ef1ae1bfcb5daefa3e8bc374aaafb4db35
Jonathan Kim <jkimbo@gmail.com>
2019-08-16 14:36:42 +0100

    v2.1.8
    Update version to v2.1.8

--- graphene/__init__.py
+++ graphene/__init__.py
@@ -43,7 +43,7 @@ from .utils.resolve_only_args import resolve_only_args
 from .utils.module_loading import lazy_import
 
 
-VERSION = (2, 1, 7, "final", 0)
+VERSION = (2, 1, 8, "final", 0)
 
 __version__ = get_version(VERSION)
 
