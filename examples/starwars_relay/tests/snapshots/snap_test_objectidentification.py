ec07b41ec029b03b357c14c819bb61b10c856521
Jonathan Kim <jkimbo@gmail.com>
2019-12-26 12:13:42 +0000

    Update snapshots

--- examples/starwars_relay/tests/snapshots/snap_test_objectidentification.py
+++ examples/starwars_relay/tests/snapshots/snap_test_objectidentification.py
@@ -4,33 +4,55 @@ from __future__ import unicode_literals
 
 from snapshottest import Snapshot
 
+
 snapshots = Snapshot()
 
-snapshots["test_correctly_fetches_id_name_rebels 1"] = {
-    "data": {
-        "rebels": {"id": "RmFjdGlvbjox", "name": "Alliance to Restore the Republic"}
+snapshots['test_correctly_fetches_id_name_rebels 1'] = {
+    'data': {
+        'rebels': {
+            'id': 'RmFjdGlvbjox',
+            'name': 'Alliance to Restore the Republic'
+        }
     }
 }
 
-snapshots["test_correctly_refetches_rebels 1"] = {
-    "data": {"node": {"id": "RmFjdGlvbjox", "name": "Alliance to Restore the Republic"}}
+snapshots['test_correctly_refetches_rebels 1'] = {
+    'data': {
+        'node': {
+            'id': 'RmFjdGlvbjox',
+            'name': 'Alliance to Restore the Republic'
+        }
+    }
 }
 
-snapshots["test_correctly_fetches_id_name_empire 1"] = {
-    "data": {"empire": {"id": "RmFjdGlvbjoy", "name": "Galactic Empire"}}
+snapshots['test_correctly_fetches_id_name_empire 1'] = {
+    'data': {
+        'empire': {
+            'id': 'RmFjdGlvbjoy',
+            'name': 'Galactic Empire'
+        }
+    }
 }
 
-snapshots["test_correctly_refetches_empire 1"] = {
-    "data": {"node": {"id": "RmFjdGlvbjoy", "name": "Galactic Empire"}}
+snapshots['test_correctly_refetches_empire 1'] = {
+    'data': {
+        'node': {
+            'id': 'RmFjdGlvbjoy',
+            'name': 'Galactic Empire'
+        }
+    }
 }
 
-snapshots["test_correctly_refetches_xwing 1"] = {
-    "data": {"node": {"id": "U2hpcDox", "name": "X-Wing"}}
+snapshots['test_correctly_refetches_xwing 1'] = {
+    'data': {
+        'node': {
+            'id': 'U2hpcDox',
+            'name': 'X-Wing'
+        }
+    }
 }
 
-snapshots[
-    "test_str_schema 1"
-] = '''"""A faction in the Star Wars saga"""
+snapshots['test_str_schema 1'] = '''"""A faction in the Star Wars saga"""
 type Faction implements Node {
   """The ID of the object"""
   id: ID!
@@ -84,9 +106,10 @@ type PageInfo {
 type Query {
   rebels: Faction
   empire: Faction
-
-  """The ID of the object"""
-  node(id: ID!): Node
+  node(
+    """The ID of the object"""
+    id: ID!
+  ): Node
 }
 
 """A ship in the Star Wars saga"""
