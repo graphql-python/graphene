796880fc5cc1d79976f95390af00b0798201c9c3
Christoph Zwerschke <cito@online.de>
2020-03-04 11:24:42 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -45,21 +45,20 @@ class PyTest(TestCommand):
 
 
 tests_require = [
-    "pytest",
-    "pytest-benchmark",
-    "pytest-cov",
-    "pytest-mock",
-    "pytest-asyncio",
-    "snapshottest",
-    "coveralls",
-    "promise",
-    "six",
-    "mock",
-    "pytz",
-    "iso8601",
+    "pytest>=5.3,<6",
+    "pytest-benchmark>=3.2,<4",
+    "pytest-cov>=2.8,<3",
+    "pytest-mock>=2,<3",
+    "pytest-asyncio>=0.10,<2",
+    "snapshottest>=0.5,<1",
+    "coveralls>=1.11,<2",
+    "promise>=2.3,<3",
+    "mock>=4.0,<5",
+    "pytz==2019.3",
+    "iso8601>=0.1,<2",
 ]
 
-dev_requires = ["black==19.3b0", "flake8==3.7.7"] + tests_require
+dev_requires = ["black==19.10b0", "flake8>=3.7,<4"] + tests_require
 
 setup(
     name="graphene",
@@ -83,9 +82,9 @@ setup(
     keywords="api graphql protocol rest relay graphene",
     packages=find_packages(exclude=["tests", "tests.*", "examples"]),
     install_requires=[
-        "graphql-core>=3.0.3,<4",
-        "graphql-relay>=3.0.0,<4",
-        "aniso8601>=6,<9",
+        "graphql-core>=3.0.3,<3.1",
+        "graphql-relay>=3.0,<4",
+        "aniso8601>=8,<9",
         "unidecode>=1.1.1,<2",
     ],
     tests_require=tests_require,
