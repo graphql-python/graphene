a2872f2a920ae3b5c67f4901e3ac28fdeb954d34
Minh Tu Le <minhtu@twingate.com>
2021-04-17 15:30:08 -0700

    Use argument's default value regardless if the input field is required

--- graphene/types/schema.py
+++ graphene/types/schema.py
@@ -313,9 +313,7 @@ class TypeMap(dict):
                         arg_type,
                         out_name=arg_name,
                         description=arg.description,
-                        default_value=Undefined
-                        if isinstance(arg.type, NonNull)
-                        else arg.default_value,
+                        default_value=arg.default_value,
                     )
                 subscribe = field.wrap_subscribe(
                     self.get_function_for_type(
