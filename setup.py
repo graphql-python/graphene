45605b028ea9a86c7ce4d692b69277a6315f2511
ddelange <14880945+ddelange@users.noreply.github.com>
2022-04-04 13:50:28 +0200

    Disallow 3.3 pro-actively

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     keywords="api graphql protocol rest relay graphene",
     packages=find_packages(exclude=["examples*"]),
     install_requires=[
-        "graphql-core~=3.1",
+        "graphql-core>=3.1.2,<3.3",
         "graphql-relay>=3.0,<4",
         "aniso8601>=8,<10",
     ],
