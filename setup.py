221afaf4c441a5e88e039440808d4810d5f040e7
Dulmandakh <dulmandakh@gmail.com>
2024-05-16 16:17:46 +0800

    bump pytest to 7 (#1546)
    * bump pytest
    
    * downgrade pytest-cov

--- setup.py
+++ setup.py
@@ -45,9 +45,9 @@ class PyTest(TestCommand):
 
 
 tests_require = [
-    "pytest>=6,<7",
-    "pytest-benchmark>=3.4,<4",
-    "pytest-cov>=3,<4",
+    "pytest>=7,<8",
+    "pytest-benchmark>=4,<5",
+    "pytest-cov>=4,<5",
     "pytest-mock>=3,<4",
     "pytest-asyncio>=0.16,<2",
     "snapshottest>=0.6,<1",
