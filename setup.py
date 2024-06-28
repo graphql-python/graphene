d324fadfa743de521be86f47e0d42f5d4c35045d
Dulmandakh <dulmandakh@gmail.com>
2024-06-28 21:11:36 +0800

    CI: fix tests on Python 3.13

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ tests_require = [
     "pytest-mock>=3,<4",
     "pytest-asyncio>=0.16,<2",
     "snapshottest>=0.6,<1",
-    "coveralls>=4,<5",
+    "coveralls>=3.3,<5",
 ]
 
 dev_requires = ["ruff==0.4.10", "flake8>=4,<5"] + tests_require
