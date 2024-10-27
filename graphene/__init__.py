ccae7364e572dbb1f08c980d79fae0134bd0263a
Erik Wrede <erikwrede@users.noreply.github.com>
2024-10-27 21:16:40 +0100

    release: 3.4.1

--- graphene/__init__.py
+++ graphene/__init__.py
@@ -46,7 +46,7 @@ from .types import (
 from .utils.module_loading import lazy_import
 from .utils.resolve_only_args import resolve_only_args
 
-VERSION = (3, 4, 0, "final", 0)
+VERSION = (3, 4, 1, "final", 0)
 
 
 __version__ = get_version(VERSION)
